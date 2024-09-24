70b098695763c24ecbb12a78400bfbdf5f545765
Mukund Kapadia <mdrkkapadia@gmail.com>
2024-09-24 14:34:36 -0400

    requests logs and counters added

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-    console.log("Get Request")
+let getRequest = 0
+let postRequest = 0
+
+const products = [
+    
+]
+
+app.get('/products', (req, res) => {
+    getRequest++
+    console.log("products GET: received request")
+    console.log("products GET: sending response")
+
+    console.log(`Processed Request Count--> GET:${getRequest}, POST: ${postRequest}`)
+    res.json({"message":"Success"})
+    
 })
 
 app.listen(port,()=>{
-    console.log("Server listening on port: " + port)
+    console.log(`Server listening at: http://localhost:${port} `)
 })
